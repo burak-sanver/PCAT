585a004e1f7a1938d997a76b92ff90f6f2900ecb
Your Name <you@example.com>
2022-06-23 14:30:53 +0300

    uploaded

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const express = require('express');
-const path = require('path')
+const mongoose = require('mongoose');
+
+const ejs = require('ejs');
+const path = require('path');
+
+const Photo = require('./models/Photo')
+
+
 const app = express();
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  
+// ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  //res.render('about');
+  //console.log(req.params.id)
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo', { photo })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 const port = 3000;
